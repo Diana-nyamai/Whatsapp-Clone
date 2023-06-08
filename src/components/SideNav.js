bf05846d7ebed980eef7a13157a4dc04fcb08b14
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-08 16:58:21 +0300

    Side navigation component completed

---
+++ src/components/SideNav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/components/sidenav.css";
+import TopNav from "./TopNav";
+
+function SideNav() {
+  return (
+    <>
+      <div className="sidepane">
+        <TopNav />
+      </div>
+    </>
+  );
+}
+
+export default SideNav;
