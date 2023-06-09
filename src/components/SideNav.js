c08a7c1210d21cd5de16d245743746c13b39cd33
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-09 06:40:23 +0300

    Side navigation update

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/components/sidenav.css";
 import TopNav from "./TopNav";
+import SearchBar from "./SearchBar";
+import ChatList from "./ChatList";
 
 function SideNav() {
   return (
     <>
       <div className="sidepane">
         <TopNav />
+        <SearchBar />
+        <ChatList/>
       </div>
     </>
   );
