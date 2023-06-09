42f4f2b94a5d9a515a94a1532683bb13f687d8b3
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-09 08:46:49 +0300

    Update: handling searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/searchbar.css";
 import { AiOutlineSearch } from "react-icons/ai";
+import {AiOutlineArrowLeft} from 'react-icons/ai'
 
 function SearchBar() {
+  const [isArrowVisible, setisArrowVisible] = useState(false)
+
+  const  handleArrowChange = () => setisArrowVisible(true)
+  const handleArrowClick = () => setisArrowVisible(false)
   return (
     <div className="searchbar">
       <div className="input-b">
-        <input type="text" placeholder="Search or start a new chat" />
-        <div className="search-icon">
-          <AiOutlineSearch />
+        <input onClick={handleArrowChange} type="text" placeholder="Search or start a new chat" />
+        <div className="search-icon" onClick={handleArrowClick}>
+          {isArrowVisible? <AiOutlineArrowLeft/> : <AiOutlineSearch/>}
         </div>
       </div>
     </div>
