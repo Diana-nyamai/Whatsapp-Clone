546427a75da2fc7a24465c4a8ec27151343995c5
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-09 06:40:53 +0300

    Top navigation update

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -2,12 +2,13 @@ import React from "react";
 import "../styles/components/topnav.css";
 import Avatars from "./Avatars";
 import SideIcons from "./SideIcons";
+import profile from "../assets/profile.jpg";
 
 function TopNav() {
   return (
     <>
       <div className="topnav">
-        <Avatars />
+        <Avatars image={profile} awidth={40} aheight={40} />
         <SideIcons />
       </div>
     </>
