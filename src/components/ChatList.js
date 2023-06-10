8559bf17ceaa3b3ebbdca84025f47391d8afb795
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-10 09:01:55 +0300

    Update: added a notification prompt

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -2,11 +2,6 @@ import React from "react";
 import "../styles/components/chatlist.css";
 import Avatars from "./Avatars";
 import profile from "../assets/profile.jpg";
-// import p2 from "../assets/p2.jpg";
-// import p3 from "../assets/p3.jpg";
-// import p4 from "../assets/p4.jpg";
-// import p5 from "../assets/p5.jpg";
-// import p6 from "../assets/p6.jpg";
 
 function ChatList() {
   const chatLists = [
@@ -39,12 +34,20 @@ function ChatList() {
       userName: "Abraham Stewart",
       message: "Hello there, how are you?",
       time: "Tuesday",
+      notify: '2',
+      notclass: "notify",
+      green: "green",
+      black: "black"
     },
     {
       avatar: <Avatars image={profile} awidth={50} aheight={50} />,
       userName: "Jill Pope",
       message: "Hello there, how are you?",
       time: "Tuesday",
+      notify: '1',
+      notclass: "notify",
+      green: "green",
+      black: "black"
     },
     {
       avatar: <Avatars image={profile} awidth={50} aheight={50} />,
@@ -80,20 +83,24 @@ function ChatList() {
   return (
     <div className="chatlist-main">
       <div className="chatlist-main">
-      {chatLists.map((chatlist) => (
-        <div className="chatlist-container">
-          <div className="chat-list">
-            <div className="chatlist-avatar">{chatlist.avatar}</div>
-            <div className="c-underline">
-              <div className="c-text">
-                <p className="name">{chatlist.userName}</p>
-                <p className="message">{chatlist.message}</p>
+        {chatLists.map((chatlist) => (
+          <div className="chatlist-container">
+            <div className="chat-list">
+              <div className="chatlist-avatar">{chatlist.avatar}</div>
+              <div className="c-underline">
+                <div className="c-text">
+                  <p className={`name ${chatlist.black}`}>{chatlist.userName}</p>
+                  <p className="message">{chatlist.message}</p>
+                </div>
+                <div className={`c-time ${chatlist.green}`}>
+                  <p>{chatlist.time}</p>
+                  <p className={chatlist.notclass}>{chatlist.notify}</p>
+                  </div>
+
               </div>
-              <div className="c-time">{chatlist.time}</div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
