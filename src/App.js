3d507e39fd600df2a75fd7e7679b0c5fa7862b53
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-10 09:02:22 +0300

    Update: added a new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import DefaultContent from "./components/DefaultContent";
 import SideNav from "./components/SideNav";
 
 function App() {
   return (
-    <div>
-      <SideNav/>
+    <div className="App">
+      <SideNav />
+      <DefaultContent />
     </div>
   );
 }
-
 export default App;
